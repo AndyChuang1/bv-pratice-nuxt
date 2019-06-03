e1652cb20414d2046fa11302585736884da326cb
Andychuang <bboy81905@gmail.com>
2019-06-03 16:31:50 +0800

    first commit

---
+++ pages/carousel.vue
@@ -0,0 +1,215 @@
+<template>
+  <div>
+    <div class="container">
+      <div class="default-carousel">
+        <h1>Default carousel</h1>
+        <b-carousel
+          id="carousel-1"
+          v-model="slide"
+          :interval="4000"
+          controls
+          indicators
+          background="#ababab"
+          img-width="1024"
+          img-height="480"
+          style="text-shadow: 1px 1px 2px #333;"
+          @sliding-start="onSlideStart"
+          @sliding-end="onSlideEnd"
+        >
+          <!-- Text slides with image -->
+          <b-carousel-slide
+            caption="First slide"
+            text="Nulla vitae elit libero, a pharetra augue mollis interdum."
+            img-src="https://picsum.photos/1024/480/?image=52"
+          ></b-carousel-slide>
+
+          <!-- Slides with custom text -->
+          <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
+            <h1>Hello world!</h1>
+          </b-carousel-slide>
+
+          <!-- Slides with image only -->
+          <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58"></b-carousel-slide>
+
+          <!-- Slides with img slot -->
+          <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
+          <b-carousel-slide>
+            <img
+              slot="img"
+              class="d-block img-fluid w-100"
+              width="1024"
+              height="480"
+              src="https://picsum.photos/1024/480/?image=55"
+              alt="image slot"
+            >
+          </b-carousel-slide>
+
+          <!-- Slide with blank fluid image to maintain slide aspect ratio -->
+          <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros felis, tincidunt
+              a tincidunt eget, convallis vel est. Ut pellentesque ut lacus vel interdum.
+            </p>
+          </b-carousel-slide>
+        </b-carousel>
+      </div>
+    </div>
+    <div class="card-carousel container">
+      <div>
+        <h1>Card-carousel</h1>
+        <b-carousel
+          id="carousel-1"
+          v-model="slide2"
+          controls
+          indicators
+          background="#ababab"
+          img-width="1000"
+          img-height="300"
+          style="text-shadow: 1px 1px 2px #333;"
+        >
+          <b-carousel-slide img-blank img-blank-color="#f5f5f5">
+            <b-row>
+              <b-col cols="12" lg="4">
+                <b-card
+                  title="APL-500"
+                  :img-src="require('../assets/APL-500_LIVA.png')"
+                  img-alt="Image"
+                  img-top
+                  tag="article"
+                  style="max-width: 20rem;"
+                  class="mb-2"
+                ></b-card>
+              </b-col>
+              <b-col cols="12" lg="4">
+                <b-card
+                  title="APL-500"
+                  :img-src="require('../assets/APL-500_LIVA.png')"
+                  img-alt="Image"
+                  img-top
+                  tag="article"
+                  style="max-width: 20rem;"
+                  class="mb-2"
+                ></b-card>
+              </b-col>
+              <b-col cols="12" lg="4">
+                <b-card
+                  title="APL-500"
+                  :img-src="require('../assets/APL-500_LIVA.png')"
+                  img-alt="Image"
+                  img-top
+                  tag="article"
+                  style="max-width: 20rem;"
+                  class="mb-2"
+                ></b-card>
+              </b-col>
+            </b-row>
+          </b-carousel-slide>
+          <b-carousel-slide img-blank-color="#f5f5f5" img-blank>
+            <b-row>
+              <b-col cols="12" lg="4">
+                <b-card
+                  title="APL-500"
+                  :img-src="require('../assets/APL-500_LIVA.png')"
+                  img-alt="Image"
+                  img-top
+                  tag="article"
+                  style="max-width: 20rem;"
+                  class="mb-2"
+                ></b-card>
+              </b-col>
+              <b-col cols="12" lg="4">
+                <b-card
+                  title="APL-500"
+                  :img-src="require('../assets/APL-500_LIVA.png')"
+                  img-alt="Image"
+                  img-top
+                  tag="article"
+                  style="max-width: 20rem;"
+                  class="mb-2"
+                ></b-card>
+              </b-col>
+              <b-col cols="12" lg="4">
+                <b-card
+                  title="APL-500"
+                  :img-src="require('../assets/APL-500_LIVA.png')"
+                  img-alt="Image"
+                  img-top
+                  tag="article"
+                  style="max-width: 20rem;"
+                  class="mb-2"
+                ></b-card>
+              </b-col>
+            </b-row>
+          </b-carousel-slide>
+        </b-carousel>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+
+export default {
+  components: {
+    Logo
+  },
+  data() {
+    return {
+      slide: 0,
+      slide2: 0,
+      sliding: null
+    };
+  },
+  methods: {
+    onSlideStart(slide) {
+      this.sliding = true;
+    },
+    onSlideEnd(slide) {
+      this.sliding = false;
+    }
+  }
+};
+</script>
+
+<style lang='scss'>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.card-carousel {
+  .carousel-control-prev-icon {
+    background-image: url("data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='%234dffff' viewBox='0 0 8 8'%3E%3Cpath d='M5.25 0l-4 4 4 4 1.5-1.5-2.5-2.5 2.5-2.5-1.5-1.5z'/%3E%3C/svg%3E") !important;
+  }
+
+  .carousel-control-next-icon {
+    background-image: url("data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' fill='%234dffff' viewBox='0 0 8 8'%3E%3Cpath d='M2.75 0l-1.5 1.5 2.5 2.5-2.5 2.5 1.5 1.5 4-4-4-4z'/%3E%3C/svg%3E") !important;
+  }
+  .carousel-indicators li{
+    background-color: #4dffff;
+  }
+}
+</style>
